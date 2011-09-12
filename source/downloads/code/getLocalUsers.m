c701cb5b62b75e39c459ff447b56333356802a42
Patrick Neave <patrick@milhouse.co.uk>
2011-09-12 21:59:19 +0100

    Lots of small cosmetic changes. Change font size. Make page slightly narrower.

--- source/downloads/code/getLocalUsers.m
+++ source/downloads/code/getLocalUsers.m
@@ -1,53 +1,53 @@
 
 -(NSArray*)getLocalUsers
 {
-    NSMutableArray *users = [[NSMutableArray alloc] init];
+   NSMutableArray *users = [[NSMutableArray alloc] init];
 
-    NSTask *task;
-    task = [[NSTask alloc] init];
-    [task setLaunchPath: @"/usr/bin/dscl"];
+   NSTask *task;
+   task = [[NSTask alloc] init];
+   [task setLaunchPath: @"/usr/bin/dscl"];
 
-    NSArray *arguments;
-    arguments =
-        [NSArray arrayWithObjects: @"localhost", @"list", @"/Search/Users", nil];
-    [task setArguments: arguments];
+   NSArray *arguments;
+   arguments =
+      [NSArray arrayWithObjects: @"localhost", @"list", @"/Search/Users", nil];
+   [task setArguments: arguments];
 
-    NSPipe *pipe;
-    pipe = [NSPipe pipe];
-    [task setStandardOutput: pipe];
+   NSPipe *pipe;
+   pipe = [NSPipe pipe];
+   [task setStandardOutput: pipe];
 
-    NSFileHandle *file;
-    file = [pipe fileHandleForReading];
+   NSFileHandle *file;
+   file = [pipe fileHandleForReading];
 
-    [task launch];
+   [task launch];
 
-    NSData *data;
-    data = [file readDataToEndOfFile];
+   NSData *data;
+   data = [file readDataToEndOfFile];
 
-    NSString *string;
-    string = [[NSString alloc] initWithData: data encoding:NSUTF8StringEncoding];
+   NSString *string;
+   string = [[NSString alloc] initWithData: data encoding:NSUTF8StringEncoding];
 
-    NSScanner *theScanner = [NSScanner scannerWithString:string];
+   NSScanner *theScanner = [NSScanner scannerWithString:string];
 
-    while (NO == [theScanner isAtEnd])
-    {
-        NSString *str;
-        struct passwd *userInfo;
+   while (NO == [theScanner isAtEnd])
+   {
+      NSString *str;
+      struct passwd *userInfo;
     
-        if ([theScanner scanUpToCharactersFromSet:
-                [NSCharacterSet whitespaceAndNewlineCharacterSet]
-                intoString:&str])
-        {
-            userInfo = getpwnam([str UTF8String]);
-            if (userInfo->pw_uid > 500)
-            {
-                [users addObject: str];
-            }
-        }
-    }
+      if ([theScanner scanUpToCharactersFromSet:
+            [NSCharacterSet whitespaceAndNewlineCharacterSet]
+            intoString:&str])
+      {
+         userInfo = getpwnam([str UTF8String]);
+         if (userInfo->pw_uid > 500)
+         {
+            [users addObject: str];
+         }
+      }
+   }
 
-    [string release];
-    [task release];
+   [string release];
+   [task release];
 
-    return [users autorelease];
+   return [users autorelease];
 }
